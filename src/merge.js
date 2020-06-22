32b80e5d27e66b1a942e8e6095454255ad6048b4
Sung Castro <35593158+sungkimcastro@users.noreply.github.com>
2020-06-22 17:21:51 +0300

    Update merge.js

--- src/merge.js
+++ src/merge.js
@@ -2,42 +2,41 @@ import chart, { visua } from "./chart";
 import { sleep } from "./helpers";
 
 export async function sort(arr1, arr2, arrcl) {
-  let collector = [];
-  let i = 0;
-  let j = 0;
+	let collector = [];
+	let i = 0;
+	let j = 0;
 
-  while (i < arr1.length && j < arr2.length) {
-    if (arr1[i] > arr2[j]) {
-      collector.push(arr2[j]);
-      j++;
-    } else {
-      collector.push(arr1[i]);
-      i++;
-    }
-  }
+	while (i < arr1.length && j < arr2.length) {
+		if (arr1[i] > arr2[j]) {
+			collector.push(arr2[j]);
+			j++;
+		} else {
+			collector.push(arr1[i]);
+			i++;
+		}
+	}
 
-  while (i < arr1.length) {
-    collector.push(arr1[i]);
-    i++;
-  }
-  while (j < arr2.length) {
-    collector.push(arr2[j]);
-    j++;
-  }
+	while (i < arr1.length) {
+		collector.push(arr1[i]);
+		i++;
+	}
+	while (j < arr2.length) {
+		collector.push(arr2[j]);
+		j++;
+	}
 
-  await sleep(chart.options.speed);
-
-  await visua(chart, collector, "Merge sort", arrcl);
-
-  return collector;
+	const newCollection = collector.concat(arrcl.slice(collector.length));
+	await visua(chart, newCollection, "Merge sort");
+	await sleep(chart.options.speed);
+	return collector;
 }
 
 export default async function mergeSort(arr, clone) {
-  if (arr.length <= 1) return arr;
+	if (arr.length <= 1) return arr;
 
-  const middle = Math.floor(arr.length / 2);
-  let left = await mergeSort(arr.slice(0, middle), clone);
-  let right = await mergeSort(arr.slice(middle), clone);
+	const middle = Math.floor(arr.length / 2);
+	let left = await mergeSort(arr.slice(0, middle), clone);
+	let right = await mergeSort(arr.slice(middle), clone);
 
-  return await sort(left, right, clone);
+	return await sort(left, right, clone);
 }
